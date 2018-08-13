d2e906b72dea90353f2432c34b50c973e9a32363
曾小梅 <zeng@cengxiaomeideiMac.local>
2018-08-13 19:30:20 +0800

    mmm

---
+++ ZASpec2/ZRLog.h
@@ -0,0 +1,15 @@
+//
+//  ZRLog.h
+//  zhenaiwang
+//
+//  Created by 曾小梅 on 2018/8/9.
+//  Copyright © 2018年 ZhenAiWang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZRLog : NSObject
+
++ (void)logString:(NSString *)string;
+
+@end
---
+++ ZASpec2/ZRLog.m
@@ -0,0 +1,17 @@
+//
+//  ZRLog.m
+//  zhenaiwang
+//
+//  Created by 曾小梅 on 2018/8/9.
+//  Copyright © 2018年 ZhenAiWang. All rights reserved.
+//
+
+#import "ZRLog.h"
+
+@implementation ZRLog
+
++ (void)logString:(NSString *)string {
+    NSLog(@"----------string----------:%@", string);
+}
+
+@end
